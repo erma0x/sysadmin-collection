a39cf13028b2dd79fce6399c977d26d29cf9776b
erma0x <35337869+erma0x@users.noreply.github.com>
2022-09-03 19:51:28 +0200

    Update ping_internet.py

--- ping_internet.py
+++ ping_internet.py
@@ -17,12 +17,9 @@ def ping(host):
 
 if __name__ == "__main__":
    
-   list_ip = [ ]
-
    for i in range(0,256):
        for j in range(0,256): 
            for k in range(0,256):
                for y in range(0,256):
-                   list_ip.append(str(i)+'.'+str(j)+'.'+str(k)+'.'+str(y))
-                   ping(list_ip[-1])
+                   ping( str(i) + '.' + str(j) + '.' + str(k) + '.' + str(y) )
 
